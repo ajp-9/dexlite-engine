8772ef9f3e43a2a34971ed9170bdf0b8ef8b51ab
AJP-9 <albertpjzzo@gmail.com>
2021-03-07 01:16:54 -0500

    Added premake5 & a build system for vs2019

---
+++ premake5.lua
@@ -0,0 +1,82 @@
+workspace "Zip-Engine"
+    architecture "x64"
+    startproject "Game"
+
+    configurations {"Debug", "Release"}
+
+project "Zip-Engine"
+    location "Zip-Engine"
+    kind "StaticLib"
+    language "C++"
+
+    targetdir ("%{prj.name}/bin/%{cfg.buildcfg}")
+    objdir ("%{prj.name}/intermediates/%{cfg.buildcfg}")
+
+    files
+    {
+        "%{prj.location}/src/**.hpp",
+        "%{prj.location}/src/**.cpp"
+    }
+
+    sysincludedirs
+    {
+        "src",
+        "%{wks.location}/linking/include/"
+    }
+
+    syslibdirs
+    {
+        "%{wks.location}/linking/libs"
+    }
+
+    links
+    {
+        "SDL2-static.lib"
+    }
+
+    filter "configurations:Debug"
+        optimize "Off"
+
+    filter "configurations:Release"
+        optimize "On"
+
+project "Game"
+    location "Game"
+    kind "ConsoleApp"
+    language "C++"
+
+    targetdir ("%{prj.name}/bin/%{cfg.buildcfg}")
+    objdir ("%{prj.name}/intermediates/%{cfg.buildcfg}")
+
+    files
+    {
+        "%{prj.location}/src/**.hpp",
+        "%{prj.location}/src/**.cpp"
+    }
+
+    sysincludedirs
+    {
+        "src",
+        "%{wks.location}/linking/include/",
+        "%{wks.location}/Zip-Engine/src"
+    }
+    
+    syslibdirs
+    {
+        "%{wks.location}/linking/libs"
+    }
+
+    links
+    {
+        "Zip-Engine",
+        "winmm.lib",
+        "imm32.lib",
+        "version.lib",
+        "setupapi.lib"
+    }
+
+    filter "configurations:Debug"
+        optimize "Off"
+
+    filter "configurations:Release"
+        optimize "On"
